a5ae3e69d8aae015d9c8421dc13dafb28a10acec
Mehedi <mehedi25122003@gmail.com>
2025-06-21 21:53:05 +0600

    error page edited

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -2,7 +2,7 @@ import error from '../images/original-2e27796737e975dc1e453c3b72df2a3d.gif'
 
 const Error = () => {
     return (
-        <div>
+        <div className='min-h-screen flex items-center justify-center'>
             <img src={error} alt="" />
         </div>
     );
