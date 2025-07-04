3459deddf4126c60228bbba6f57f78eb92681151
mehediScriptDev <mehedi25122003@gmail.com>
2025-07-04 20:00:40 +0600

    register page almost done

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,37 +1,94 @@
-import login from '../images/Login.gif'
+import login from "../images/Login.gif";
 
 const Register = () => {
-    const handlesubmit =e=>{
-        e.target.preventDefault()
-    }
+  const handlesubmit = (e) => {
+    e.target.preventDefault();
+  };
   return (
     <div>
-            <div className='flex justify-center items-center'>
-                 
-            </div>
+      <div className="flex justify-center items-center"></div>
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content gap-2 flex-col lg:flex-row-reverse">
-       
           <div>
             <img src={login} alt="loginimage" />
           </div>
           <div className="card w-full max-w-sm shrink-0">
+            <div className="flex justify-center flex-col items-center space-y-2">
+              <h1 className="text-xs text-primary">Register</h1>
+            <h1 className="text-3xl font-bold">Start for free today</h1>
+            <p className="text-gray-500">
+              Access to all features. No credit card required.
+            </p>
+            <button className="btn w-full bg-white text-black border-[#e5e5e5]">
+              <svg
+                aria-label="Google logo"
+                width="16"
+                height="16"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <g>
+                  <path d="m0 0H512V512H0" fill="#fff"></path>
+                  <path
+                    fill="#34a853"
+                    d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                  ></path>
+                  <path
+                    fill="#4285f4"
+                    d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                  ></path>
+                  <path
+                    fill="#fbbc02"
+                    d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                  ></path>
+                  <path
+                    fill="#ea4335"
+                    d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                  ></path>
+                </g>
+              </svg>
+              Login with Google
+            </button>
+            </div>
             <div className="card-body">
-              <fieldset onSubmit={handlesubmit} className="fieldset">
-                <label className="label">Email</label>
-                <input name="email" type="email" className="input" placeholder="Email" />
-                <label className="label">Email</label>
-                <input name="email" type="email" className="input" placeholder="Email" />
-                <label className="label">Email</label>
-                <input name="email" type="email" className="input" placeholder="Email" />
-                <label className="label">Password</label>
+              <fieldset onSubmit={handlesubmit} className="fieldset space-y-1">
+                <label className="label text-xl text-black ">
+                  Full Name:
+                </label>
                 <input
-                name="password"
-                  type="password"
-                  className="input"
-                  placeholder="Password"
+                  name="name"
+                  type="email"
+                  className="input py-3"
+                  placeholder="Type your full name here"
                 />
-                
+                <label className="label text-xl text-black ">
+                  Username:
+                </label>
+                <input
+                  name="username"
+                  type="email"
+                  className="input py-3"
+                  placeholder="Type your username here"
+                />
+                <label className="label text-xl text-black ">
+                  Email:
+                </label>
+                <input
+                  name="email"
+                  type="email"
+                  className="input py-3"
+                  placeholder="Type your Email here"
+                />
+                <label className="label text-xl text-black ">
+                  Password:
+                </label>
+                <input
+                  name="password"
+                  type="password"
+                  className="input py-3"
+                  placeholder="Type your Password here"
+                />
+
                 <button className="btn btn-primary mt-4">Login</button>
               </fieldset>
             </div>
