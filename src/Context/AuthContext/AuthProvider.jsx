7220bd86812a15d4271b26d18828ecaf60921c2d
mehediScriptDev <mehedi25122003@gmail.com>
2025-07-06 18:42:50 +0600

    setLodaing

--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import AuthContext from './AuthContext';
 import { CgPassword } from 'react-icons/cg';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import auth from '../../Firebase/firebase.init';
 
 const AuthProvider = ({children}) => {
     const [user,setUser] = useState(null);
@@ -16,7 +17,8 @@ const AuthProvider = ({children}) => {
     }
 
     const createuser = (email,password)=>{
-        return createUserWithEmailAndPassword;
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password);
     }
     return (
         <AuthContext.Provider value={authinfo}>
