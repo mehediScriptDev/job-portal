c5dd2fe5a8f6c0ac36b1969117f783075ada6732
mehediScriptDev <mehedi25122003@gmail.com>
2025-07-07 21:55:04 +0600

    onauthstatechanged added

--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AuthContext from './AuthContext';
 import { CgPassword } from 'react-icons/cg';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 import auth from '../../Firebase/firebase.init';
 
 const AuthProvider = ({children}) => {
@@ -12,6 +12,15 @@ const AuthProvider = ({children}) => {
         setLoading(true);
         return createUserWithEmailAndPassword(auth,email,password);
     }
+    useEffect(()=>{
+        const unsubscribe= onAuthStateChanged(auth,currentUser=>{
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return()=>{
+            unsubscribe();
+        }
+    },[])
 
     const authinfo={
         user,
