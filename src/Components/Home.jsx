b3d8f4ded545c03c3709afaab79234d8ced2b16e
mehediScriptDev <mehedi25122003@gmail.com>
2025-08-13 21:18:18 +0600

    two images animation done with framer motion for the first time

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,6 @@
-import { easeIn, easeInOut, motion as Motion } from "motion/react"
-import programmer from '../images/programmer.jpg'
-
+import { easeIn, easeInOut, motion as Motion } from "motion/react";
+import programmer from "../images/programmer.jpg";
+import programmer2 from "../images/programmer2.jpg";
 
 const Home = () => {
   return (
@@ -10,18 +10,38 @@ const Home = () => {
           <div className="hero-content flex-col lg:flex-row-reverse">
             <div className="flex-1">
               <Motion.img
-              animate={{y:[0,30,0]}}
-              transition={{duration:10,repeat:Infinity}}
-              src={programmer}
-              className="max-w-sm rounded-br-4xl rounded-t-4xl border-b-4 border-l-4 border-l-blue-400 border-b-yellow-600"
-            />
+                animate={{ y: [20, 60, 20] }}
+                transition={{ duration: 10, repeat: Infinity }}
+                src={programmer}
+                className="max-w-sm rounded-br-4xl rounded-t-4xl border-b-4 border-l-4 border-l-blue-400 border-b-yellow-600"
+              />
+              <Motion.img
+                animate={{ x: [100, 150, 100] }}
+                transition={{ duration: 10, repeat: Infinity }}
+                src={programmer2}
+                className="max-w-sm rounded-br-4xl rounded-t-4xl border-b-4 border-l-4 border-l-blue-400 border-b-yellow-600"
+              />
             </div>
             <div className="flex-1">
-              <Motion.h1 animate={{x:50}}
-              transition={{duration:3, delay:1, ease:easeInOut, repeat:Infinity}} className="text-5xl font-bold">Job portal <Motion.span
-              animate={{color:["#ff0000", "#ffff00", "#00ff00"]}}
-              transition={{duration:2,repeat:Infinity,ease:easeIn}}
-              >website</Motion.span> offer you best jobs!</Motion.h1>
+              <Motion.h1
+                animate={{ x: 50 }}
+                transition={{
+                  duration: 3,
+                  delay: 1,
+                  ease: easeInOut,
+                  repeat: Infinity,
+                }}
+                className="text-5xl font-bold"
+              >
+                Job portal{" "}
+                <Motion.span
+                  animate={{ color: ["#ff0000", "#ffff00", "#00ff00"] }}
+                  transition={{ duration: 2, repeat: Infinity, ease: easeIn }}
+                >
+                  website
+                </Motion.span>{" "}
+                offer you best jobs!
+              </Motion.h1>
               <p className="py-6">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut
                 assumenda excepturi exercitationem quasi. In deleniti eaque aut
