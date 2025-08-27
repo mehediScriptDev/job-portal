bb403c081383717d8c91619dd6141ffadab22741
mehediScriptDev <mehedi25122003@gmail.com>
2025-08-27 22:51:59 +0600

    sweet alert added

--- src/Components/ApplyJob.jsx
+++ src/Components/ApplyJob.jsx
@@ -1,30 +1,65 @@
+import Swal from "sweetalert2";
 const ApplyJob = () => {
+  const submitForm = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const linkedin = form.linkedin.value;
+    const github = form.github.value;
+    const resume = form.resume.value;
 
-    const submitForm = (e) => {
-        e.preventDefault();
-        const form = e.target;
-        const linkedin = form.linkedin.value;
-        const github = form.github.value;
-        const resume = form.resume.value;
-        console.log(linkedin,github,resume)
-    }
+    const jobapplication = { linkedin, github, resume };
+    fetch("http://localhost:5000/jobapplications", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(jobapplication),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Error!",
+            text: "Do you want to continue",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+        }
+      });
+  };
+
   return (
     <div>
       <div className=" min-h-screen">
         <div className=" ">
-           
           <div className=" w-full bg-base-100  px-5 shadow-2xl">
-           <h1 className="text-3xl w-full py-1 text-center font-bold">Apply job!</h1>
+            <h1 className="text-3xl w-full py-1 text-center font-bold">
+              Apply job!
+            </h1>
             <div className="w-full">
-              <form onSubmit={submitForm} className="flex flex-col w-full">
+              <form onSubmit={submitForm} className="flex py-3 flex-col w-full">
                 <label className="label">Linkedin URL</label>
-                <input type="url" className="input w-full" name="linkedin" placeholder="Linkedin URL" />
+                <input
+                  type="url"
+                  className="input w-full"
+                  name="linkedin"
+                  placeholder="Linkedin URL"
+                />
                 <label className="label">Github URL</label>
-                <input type="url" className="input w-full" name="github" placeholder="Github URL" />
+                <input
+                  type="url"
+                  className="input w-full"
+                  name="github"
+                  placeholder="Github URL"
+                />
                 <label className="label">Resume URL</label>
-                <input type="url" className="input w-full" name="resume" placeholder="Resume URL" />
-                
-                
+                <input
+                  type="url"
+                  className="input w-full"
+                  name="resume"
+                  placeholder="Resume URL"
+                />
+
                 <button className="btn btn-primary mt-4">Apply</button>
               </form>
             </div>
