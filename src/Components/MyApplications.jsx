2a947fe1535f8ff5bfe258a70d976804fda6bb8c
Mehedi <mehedi25122003@gmail.com>
2025-09-19 17:23:41 +0600

    got all jobs

--- src/Components/MyApplications.jsx
+++ src/Components/MyApplications.jsx
@@ -22,9 +22,15 @@ const MyApplications = () => {
       <h1 className="text-primary text-5xl font-bold text-center">
         My Applications {job.length}
       </h1>
-      <div>
+      <div className="grid grid-cols-5 border-amber-100 border-2 gap-3 gap-y-2 justify-items-center py-9 mt-12
+      ">
         {job.map(singleAppication => (
-          <h1>{singleAppication.linkedin}</h1>
+         <div>
+          <img src={singleAppication.company_logo} alt="logo" />
+           <h1>{singleAppication.title}</h1>
+          <h2>{singleAppication.jobType}</h2>
+          <h3>{singleAppication.company}</h3>
+         </div>
         ))}
       </div>
     </div>
