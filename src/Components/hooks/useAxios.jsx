74f18f17fcaf09e0f439a95bcc80e3a7786dde89
Mehedi <mehedi25122003@gmail.com>
2025-09-22 16:12:09 +0600

    send this repo to  borovai fix my error

--- src/Components/hooks/useAxios.jsx
+++ src/Components/hooks/useAxios.jsx
@@ -1,11 +1,36 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router";
+import useAuth from "./useAuth";
+
+const instance = axios.create({
+  baseURL: "http://localhost:5000/",
+  withCredentials: true,
+});
 
 const useAxios = () => {
-  const instance = axios.create({
-    baseURL: "http://localhost:5000/",
-    withCredentials:true
-  });
+  const  signOUT  = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    instance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        if (error.status == 401 || error.status == 402) {
+          signOUT()
+            .then(() => {
+              navigate("/login");
+            })
+            .catch((err) => {
+              console.log(err.status);
+            });
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, []);
   return instance;
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -60,6 +60,7 @@ const router = createBrowserRouter([
     ],
   },
 ]);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
